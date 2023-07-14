9bded61633a3e871767a9f51e76e2439b014eeba
Hope Makanda <hopemwaura39@gmail.com>
2023-07-14 16:57:42 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,14 +2,14 @@
 
 /**
  * main - main function
- * 
+ *
  * Return: always 0
  */
 
 int main(void)
 {
 	char i;
-	
+
 	for (i = 'a' ; i <= 'z' ; i++)
 		putchar(i);
 	for (i = 'A' ; i <= 'Z' ; i++)
